b0ff3d95e7e43fd6a60e8ac81e3c5f97786a98c8
Aaron Jackson <yolanother@gmail.com>
2021-07-07 22:29:50 -0700

    Start adding curve functions

---
+++ Scripts/Runtime/Functions/CurveFunctionReference.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace DoubTech.EquationEvaluator.Functions
+{
+    [CreateAssetMenu(fileName = "CurveFunctionReference",
+        menuName = "Equation Evaluator/Functions/Curve")]
+    public class CurveFunctionReference : FunctionReference
+    {
+        [SerializeField] private AnimationCurve curve;
+        public override double Evaluate(double[] arguments)
+        {
+            return curve.Evaluate((float) arguments[0]);
+        }
+    }
+}
--- Scripts/Runtime/Functions/FunctionReference.cs
+++ Scripts/Runtime/Functions/FunctionReference.cs
@@ -2,7 +2,6 @@ using UnityEngine;
 
 namespace DoubTech.EquationEvaluator.Functions
 {
-    [CreateAssetMenu(fileName = "FunctionReference", menuName = "FunctionReference")]
     public abstract class FunctionReference : ScriptableObject
     {
         public string name;
